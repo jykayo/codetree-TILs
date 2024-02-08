92bc29f580ed3e0a0207075869f3a583577e53b6
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-09 00:40:19 +0900

    Title: 전화번호 바꾸기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240209/전화번호 바꾸기/change-phone-number.py
@@ -0,0 +1,6 @@
+arr = input().split("-")
+xxxx = int(arr[1])
+yyyy = int(arr[2])
+# print(f"{arr[0]}-{yyyy}-{xxxx}")
+# print("{0}-{1}-{2}".format(arr[0], yyyy, xxxx))
+print("%s-%d-%d" % (arr[0], yyyy, xxxx))

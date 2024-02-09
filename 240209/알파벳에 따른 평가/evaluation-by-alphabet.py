5df01b662ef291fdc7a82a52c3cd0e806f7fe37f
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-09 10:31:25 +0900

    Title: 알파벳에 따른 평가, Time: 123ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240209/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+alph = input()
+if alph == 'S':
+    print("Superior")
+elif alph == 'A':
+    print("Exellent")
+elif alph == 'B':
+    print("Good")
+elif alph == 'C':
+    print("Usually")
+elif alph == 'D':
+    print("Effort")
+else:
+    print("Failure")

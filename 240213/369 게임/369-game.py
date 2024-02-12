380e6f48411c8adc99a0c26bed3aede34176be90
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-13 04:47:35 +0900

    Title: 369 게임, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240213/369 게임/369-game.py
+++ 240213/369 게임/369-game.py
@@ -3,6 +3,8 @@ for i in range(1, n+1):
     if i >= 10:
         if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
             print(0, end=' ')
+        elif i % 30 >= 0 and i % 30 < 10:
+            print(0, end=' ')
         else:
             print(i, end=' ')
     elif i <= 10:

ede60ea1507c8d222bce8a72573334bcaa46e2be
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-12 19:47:00 +0900

    Title: a/b 출력, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240212/a/b 출력/a-divide-b.py
+++ 240212/a/b 출력/a-divide-b.py
@@ -1,8 +1,7 @@
 arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
-a *= 10
-print("0.", end='')
-for i in range(20):
-    print(a // b, end='')
-    a = a % b * 10
+print(f"{a/b:.1f}", end='')
+for i in range(19):
+    a = (a * 10) % b
+    print(int((a * 10) / 10), end='')

15a9df95976a93f67400e632964f1e9b04bd5fa6
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-13 04:55:52 +0900

    Title: 369 게임, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/369 게임/369-game.py
+++ 240213/369 게임/369-game.py
@@ -1,14 +1,10 @@
 n = int(input())
 for i in range(1, n+1):
-    if i >= 10:
-        if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
-            print(0, end=' ')
-        elif i % 30 >= 0 and i % 30 < 10:
-            print(0, end=' ')
-        else:
-            print(i, end=' ')
-    elif i <= 10:
-        if i % 3 == 0:
-            print(0, end=' ')
-        else:
-            print(i, end=' ')
+    if i % 3 == 0:
+        print(0, end=' ')
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print(0, end=' ')
+    elif (i // 10) % 10 == 3 or (i // 10) % 10 == 6 or (i // 10) % 10 == 9:
+        print(0, end=' ')
+    else:
+        print(i, end=' ')

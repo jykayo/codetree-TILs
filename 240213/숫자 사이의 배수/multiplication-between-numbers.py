012a98d1cfea841e87e8dc319861f00a2ead5722
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-13 20:11:18 +0900

    Title: 숫자 사이의 배수, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

--- 240213/숫자 사이의 배수/multiplication-between-numbers.py
+++ 240213/숫자 사이의 배수/multiplication-between-numbers.py
@@ -4,10 +4,7 @@ b = int(arr[1])
 sum = 0
 cnt = 0
 for i in range(a, b+1):
-    if i % 5 == 0:
-        sum += i
-        cnt += 1
-    if i % 7 == 0:
+    if i % 5 == 0 or i % 7 == 0:
         sum += i
         cnt += 1
 avg = sum / cnt

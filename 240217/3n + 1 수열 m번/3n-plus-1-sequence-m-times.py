847a167650da8f58ab55ec65b5e81c7adbf2e565
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-17 05:47:48 +0900

    Title: 3n + 1 수열 m번, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 240217/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240217/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -4,9 +4,9 @@ for i in range(m):
     cnt = 0
     while n != 1:
         if n % 2 == 0:
-            n //= 2
+            n /= 2
             cnt += 1
         else:
             n = (n * 3) + 1
             cnt += 1
-print(cnt)
+    print(cnt)

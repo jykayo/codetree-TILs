4de523deb9530aec70af3018e3b9508bbefe0790
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-17 06:43:15 +0900

    Title: 약수의 개수가 3개인 수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240217/약수의 개수가 3개인 수/square-of-prime-number.py
+++ 240217/약수의 개수가 3개인 수/square-of-prime-number.py
@@ -1,9 +1,9 @@
 arr = input().split()
 start = int(arr[0])
 end = int(arr[1])
-div = 0
 cnt = 0
 for i in range(start, end+1):
+    div = 0
     for j in range(1, i):
         if i % j == 0:
             div += 1

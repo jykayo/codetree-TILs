a81792e561fffd285bd6fd4d2ccba00afd543cd8
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-18 06:20:57 +0900

    Title: 배열 채우기, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/배열 채우기/filling-array.py
+++ 240218/배열 채우기/filling-array.py
@@ -1,7 +1,7 @@
 arr = tuple(map(int, input().split()))
 for i in range(10):
-    a = i
     if arr[i] == 0:
         break;
-for i in range(a-1, -1, -1):
+    a = i
+for i in range(a, -1, -1):
     print(arr[i], end=' ')

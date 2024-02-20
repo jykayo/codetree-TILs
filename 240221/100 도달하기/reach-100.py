be10bc1e89a1f2319a8b9d6b611eaa9039abf80c
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-21 05:03:03 +0900

    Title: 100 도달하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240221/100 도달하기/reach-100.py
+++ 240221/100 도달하기/reach-100.py
@@ -1,8 +1,10 @@
 n = int(input())
 arr = [1, n]
-for i in range(2, 10):
+i = 2
+while True:
     arr.append(arr[-2] + arr[-1])
     if arr[i] >= 100:
         break
+    i += 1
 for elem in arr:
     print(elem, end=' ')

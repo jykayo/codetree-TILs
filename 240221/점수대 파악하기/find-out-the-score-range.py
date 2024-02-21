3ece54757050365c4c562852a36b0cad73cb2c8a
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-21 11:07:06 +0900

    Title: 점수대 파악하기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,6 @@
+score_arr = list(map(int, input().split()))
+result_arr = [0] * 11
+for elem in score_arr:
+    result_arr[elem // 10] += 1
+for elem in range(10, 0, -1):
+    print(f"{elem*10} - {result_arr[elem]}")

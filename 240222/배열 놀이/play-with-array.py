a1f41bfdc2fe462182fd969b804f952553abbe62
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-22 01:53:13 +0900

    Title: 배열 놀이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240222/배열 놀이/play-with-array.py
@@ -0,0 +1,17 @@
+n, q = list(map(int, input().split()))
+n_arr = list(map(int, input().split()))
+for _ in range(q):
+    arr = list(map(int, input().split()))
+    if arr[0] == 1:
+        a = arr[1]
+        print(arr[a-1])
+    elif arr[0] == 2:
+        a = arr[1]
+        if a not in n_arr:
+            print(0)
+        else:
+            print(n_arr.index(a)+1)
+    elif arr[0] == 3:
+        a, b = arr[1], arr[2]
+        for i in range(a-1, b):
+            print(n_arr[i], end=' ')

bc9863f1bb75b0b39de249caa602f10c5ecb0fc6
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-22 12:38:46 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240222/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240222/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -5,6 +5,7 @@ for i in range(n-1):
     satis = True
     for j in range(i+1, n):
         if arr[i] == arr[j]:
+            arr[j] = 0
             satis = False
             break
     if satis == True:

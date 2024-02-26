4193f8f308c226ff6bdae69f27703188d38c65e2
jykayo <156576952+jykayo@users.noreply.github.com>
2024-02-26 15:04:34 +0900

    Title: 대문자로 바꾸기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240226/대문자로 바꾸기/change-to-capital.py
@@ -0,0 +1,9 @@
+arr = [
+	list(input().split())
+	for _ in range(5)
+]
+for i in range(5):
+	for j in range(3):
+		arr[i][j] = chr(ord(arr[i][j]) + ord('A') - ord('a'))
+		print(arr[i][j], end=' ')
+	print()

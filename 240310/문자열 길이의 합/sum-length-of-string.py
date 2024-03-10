9f6e765edaafafdd7a978d37c891c5ae3ecfa2ce
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-10 21:41:22 +0900

    Title: 문자열 길이의 합, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/문자열 길이의 합/sum-length-of-string.py
+++ 240310/문자열 길이의 합/sum-length-of-string.py
@@ -7,7 +7,6 @@ cnt = 0
 str_arr = 0
 for i in range(n):
     str_arr += len(arr[i])
-    for j in range(len(arr[i])):
-        if arr[i][j] == 'a':
-            cnt += 1
+    if arr[i][0] == 'a':
+        cnt += 1
 print(str_arr, cnt)

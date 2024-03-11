2b9724ab27cd1510331488cf3b91b8c475b681d7
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-11 14:29:20 +0900

    Title: 특정 문자의 등장 횟수, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240311/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,9 @@
+string = input()
+cnt_ee = 0
+cnt_eb = 0
+for i in range(len(string) - 1):
+    if string[i] == 'e' and string[i+1] == 'e':
+        cnt_ee += 1
+    if string[i] == 'e' and string[i+1] == 'b':
+        cnt_eb += 1
+print(cnt_ee, cnt_eb)

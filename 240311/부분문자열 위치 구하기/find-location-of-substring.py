8cfeff013c9958ee31d6e205980983c5201ff504
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-11 17:24:23 +0900

    Title: 부분문자열 위치 구하기, Time: 214ms, Memory: 24MB, Status: Passed - CodeTree

--- 240311/부분문자열 위치 구하기/find-location-of-substring.py
+++ 240311/부분문자열 위치 구하기/find-location-of-substring.py
@@ -1,14 +1,6 @@
 string = input()
 target = input()
-check = -1
-for i in range(len(string)):
-    if string[i] == target[0]:
-        for j in range(1, len(target)):
-            if string[i+j] != target[j]:
-                break
-            else:
-                check = i
-if check == -1:
-    print("No")
+if target in string:
+    print(string.index(target))
 else:
-    print(check)
+    print(-1)

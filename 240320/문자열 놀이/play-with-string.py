1f4a9f3f26044ba0738aa19d051f30e11fdf1578
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-20 23:20:41 +0900

    Title: 문자열 놀이, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240320/문자열 놀이/play-with-string.py
@@ -0,0 +1,21 @@
+s, q = input().split()
+q = int(q)
+for i in range(q):
+    num, a, b = list(input().split())
+    num = int(num)
+    if num == 1:
+        s = list(s)
+        a = int(a)
+        b = int(b)
+        s[a-1], s[b-1] = s[b-1], s[a-1]
+        s = ''.join(s)
+        print(s)
+    elif num == 2:
+        s = list(s)
+        for j in range(len(s)):
+            if s[j] == a:
+                s[j] = b
+            elif s[j] == b:
+                s[j] = a
+        s = ''.join(s)
+        print(s)

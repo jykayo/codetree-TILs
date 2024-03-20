a7bb2ea04079e4a06c27c646ec2816e1ab30a163
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-21 01:42:51 +0900

    Title: 문자열 계속 지우기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/문자열 계속 지우기/keep-removing-string.py
@@ -0,0 +1,20 @@
+strA = input()
+strB = input()
+lenA = len(strA)
+lenB = len(strB)
+while True:
+    for i in range(lenA - lenB + 1):
+        check = True
+        for j in range(lenB):
+            if strA[i+j] != strB[j]:
+                check = False
+                break
+        if check == True:
+            break
+    if check == False:
+        break
+    else:
+        for _ in range(lenA):
+            strA = strA[:i] + strA[i + lenB:]
+        lenA = len(strA)
+print(strA)

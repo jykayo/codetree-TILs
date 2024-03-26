87cb927c63c713d06407ee004437b11fac312bea
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-26 22:35:23 +0900

    Title: 정수만 추출하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/정수만 추출하기/extract-only-integers.py
+++ 240326/정수만 추출하기/extract-only-integers.py
@@ -2,13 +2,17 @@ a, b = input().split()
 check1 = 0
 check2 = 0
 for i in range(len(a)):
-    if a[i] >= '9' or a[i] <= '0':
+    if a[i] > '9' or a[i] < '0':
         check1 = i
         break
+    else:
+        check1 = len(a)
 for j in range(len(b)):
-    if b[j] >= '9' or b[j] <= '0':
+    if b[j] > '9' or b[j] < '0':
         check2 = j
         break
+    else:
+        check2 = len(b)
 str_a = a[:check1]
 str_b = b[:check2]
 

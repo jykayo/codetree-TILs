e36c1422ab334d017b00947ff5eabf61669ca418
jykayo <156576952+jykayo@users.noreply.github.com>
2024-03-28 04:20:11 +0900

    Title: 미는 횟수, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/미는 횟수/number-of-pushes.py
+++ 240328/미는 횟수/number-of-pushes.py
@@ -1,7 +1,11 @@
 string_a = input()
 string_b = input()
 cnt = 0
-while string_a != string_b:
+for _ in range(len(string_a)):
     string_a = string_a[-1] + string_a[0:-1]
     cnt += 1
+    if string_a == string_b:
+        break
+    if len(string_a) == cnt and string_a != string_b:
+        cnt = -1
 print(cnt)

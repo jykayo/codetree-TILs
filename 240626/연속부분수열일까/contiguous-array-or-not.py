a20e534dd829c77e5b0733dc3d2f05796c17360b
jykayo <156576952+jykayo@users.noreply.github.com>
2024-06-26 09:04:04 +0900

    Title: 연속부분수열일까, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240626/연속부분수열일까/contiguous-array-or-not.py
+++ 240626/연속부분수열일까/contiguous-array-or-not.py
@@ -1,14 +1,20 @@
 n1, n2 = tuple(map(int, input().split()))
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
-for i in range(n1-n2+1):
-    booli = True
-    for j in range(n2):
-        if arr1[i+j] != arr2[j]:
-            booli = False
-    if booli == True:
-        break
-if booli == True:
-    print("Yes")
-else:
+if n1 < n2:
     print("No")
+else:
+    check = False
+    for i in range(n1-n2+1):
+        booli = True
+        for j in range(n2):
+            if arr1[i+j] != arr2[j]:
+                booli = False
+                break
+        if booli == True:
+            check = True
+            break
+    if check == True:
+        print("Yes")
+    else:
+        print("No")
